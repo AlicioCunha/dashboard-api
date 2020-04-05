87bca17c5268fd26f5bc5e4175b25036aa046d16
alicio cunha <alicio.fcn@gmail.com>
2020-04-05 00:57:30 -0300

    3.09 - tratamento mensagem de exception - com arquivo de mensagem e ResponseEntityExceptionHandler

--- src/main/java/com/moneyapi/exceptionhandler/MoneyExceptionHandler.java
+++ src/main/java/com/moneyapi/exceptionhandler/MoneyExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.moneyapi.exceptionhandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +11,36 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Locale;
+
 @ControllerAdvice
-public class MoneyExceptionHandler extends ResponseEntityExceptionHandler{
+public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return  handleExceptionInternal(ex, "Mensagem inválida", headers, HttpStatus.BAD_REQUEST, request);
+        String invalidMessageUser = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
+        String invalidMessageDeveloper = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Erro(invalidMessageUser, invalidMessageDeveloper), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    public static class Erro {
+        private String messageUser;
+        private String messageDeveloper;
+
+        public Erro(String messageUser, String messageDeveloper) {
+            this.messageUser = messageUser;
+            this.messageDeveloper = messageDeveloper;
+        }
+
+        public String getMessageUser() {
+            return messageUser;
+        }
+
+        public String getMessageDeveloper() {
+            return messageDeveloper;
+        }
     }
 }
