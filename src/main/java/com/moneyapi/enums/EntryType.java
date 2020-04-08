e8ec281d38ab23e62c48caf60debaf1f43798099
alicio cunha <alicio.fcn@gmail.com>
2020-04-07 23:49:17 -0300

    5.02 - listagem e busca de lancamento

--- src/main/java/com/moneyapi/enums/EntryType.java
+++ src/main/java/com/moneyapi/enums/EntryType.java
@@ -1,8 +1,11 @@
 package com.moneyapi.enums;
 
 public enum EntryType {
+
     //Receita
     REVENUE,
+
     //DESPESA
     EXPENSE
+
 }
---
+++ src/main/java/com/moneyapi/resource/EntreyResource.java
@@ -0,0 +1,24 @@
+package com.moneyapi.resource;
+
+
+import com.moneyapi.model.Entry;
+import com.moneyapi.repository.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/entries")
+public class EntreyResource {
+
+    @Autowired
+    private EntryRepository entryRepository;
+
+    @GetMapping
+    public List<Entry> listAll() {
+        return entryRepository.findAll();
+    }
+}
